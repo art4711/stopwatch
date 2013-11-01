29de84d0b05c5e27006d01bd39b71755295fa281
Artur Grabowski <art@blahonga.org>
2013-11-01 14:34:58 +0100

    Clean this up by factoring out the same repeated calls. Add a test (failing) for Handover.

--- stopwatch_test.go
+++ stopwatch_test.go
@@ -8,37 +8,48 @@ import (
 	"time"
 )
 
-func TestSecond(t *testing.T) {
+func w1() {
 	d, _ := time.ParseDuration("1s")
-	s := New()
-	s.Start()
 	select {
 	case <- time.After(d):
 	}
-	s.Stop()
-	if s.Seconds() < 1.0 || s.Seconds() > 1.1 {
-		t.Fatalf("wrong duration: %v", s)
+}
+
+func af(t *testing.T, h, e float64, name string) {
+	if h < e || h > e + 0.1 {
+		t.Errorf("bad %v: %v !~= %v", name, h, e)
 	}
+}
+
+func TestSecond(t *testing.T) {
+	s := New()
+	s.Start()
+	w1()
+	s.Stop()
+	af(t, s.Seconds(), 1.0, "duration")
 }
 
 func TestSecondTwice(t *testing.T) {
-	d, _ := time.ParseDuration("1s")
 	s := New()
 	s.Start()
-	select {
-	case <- time.After(d):
-	}
+	w1()
 	s.Stop()
 	s.Start()
-	select {
-	case <- time.After(d):
-	}
+	w1()
 	d2 := s.Stop()
-	if s.Seconds() < 2.0 || s.Seconds() > 2.1 {
-		t.Errorf("wrong total duration: %v", s)
-	}
-	if d2.Seconds() < 1.0 || d2.Seconds() > 1.1 {
-		t.Errorf("wrong second duration: %v", s)
-	}
+	af(t, s.Seconds(), 2.0, "duration")
+	af(t, d2.Seconds(), 1.0, "stop duration")
 }
 
+func TestHandOver(t *testing.T) {
+	s1, s2 := New(), New()
+	s1.Start()
+	w1()
+	d1 := s1.Handover(&s2)
+	w1()
+	d2 := s2.Stop()
+	af(t, s1.Seconds(), 1.0, "s1 duration")
+	af(t, s2.Seconds(), 1.0, "s2 duration")
+	af(t, d1.Seconds(), 1.0, "d1 duration")
+	af(t, d2.Seconds(), 1.0, "d2 duration")
+}
