5b639e0eb4307d3e8179b9de1108450103409fe8
Artur Grabowski <art@blahonga.org>
2013-11-01 11:51:47 +0100

    Document.

--- stopwatch.go
+++ stopwatch.go
@@ -7,42 +7,61 @@ import (
 	"time"
 )
 
+// A Stopwatch is used for keeping track of elapsed wall-clock time.
+// Stopwatches don't keep track if they are running or not,
+// it's left to the user to deal with this. Unmatched Start/Stop
+// calls will lead to the stopwatch having an undefined value.
+//
+// Stopwatches are currently limited by the data type of time.Duration
+// and will not measure longer durations than 290 years.
 type Stopwatch struct {
 	acc time.Duration
 	t time.Time
 }
 
+// Returns a new Stopwatch.
 func New() Stopwatch {
 	return Stopwatch{}
 }
 
+// Starts counting elapsed wall-clock time 
 func (s *Stopwatch) Start() {
 	s.t = time.Now()
 }
 
+// Stops counting elapsed wall-clock time
 func (s *Stopwatch) Stop() {
 	s.acc = time.Since(s.t)
 }
 
+// Equivalent to s.Stop(); s2.Start() with no time elapsed between,
+// just using one time stamp.
 func (s *Stopwatch) Handover(s2 *Stopwatch) {
 	s2.t = time.Now()
 	s.acc = s.t.Sub(s2.t)
 }
 
+// Takes a snapshot of a currently running stopwatch. If the stopwatch
+// wasn't running the result is undefined.
 func (s *Stopwatch) Snapshot() Stopwatch {
 	ret := *s
 	ret.Stop()
 	return ret
 }
 
+// Reset the accumulated time in the stopwatch and 
 func (s *Stopwatch) Reset() {
 	*s = Stopwatch{}
 }
 
+// Returns the accumulated sanoseconds of the stopwatch. If the stopwatch
+// is currently running that will not be accounted for.
 func (s Stopwatch) Nanoseconds() int64 {
 	return s.acc.Nanoseconds()
 }
 
+// Returns the accumulated seconds of the stopwatch. If the stopwatch is
+// currently running that will not be accounted for.
 func (s Stopwatch) Seconds() float64 {
 	return s.acc.Seconds()
 }
